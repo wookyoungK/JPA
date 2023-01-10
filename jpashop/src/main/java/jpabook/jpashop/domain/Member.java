df85a64939d55b7f5e12bce7d9df1975cdd125e2
wookyoungK <pd1125@naver.com>
2023-01-11 00:52:27 +0900

    요구사항 분석/기본 매핑

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    /*
+    유지보수 측면
+    * setter은 무조건 만드는 것이 좋은게 아니다
+    * 가능한 생성자에서 값을 셋팅해주고 셋터의 기능을 최소화 하는 것이 좋다 여기저기서 setter를 사용하면 추적에도 안좋다
+    * */
+    @Id @GeneratedValue
+    @Column(name = "MBMBER_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
