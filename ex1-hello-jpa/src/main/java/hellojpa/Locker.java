c80dc8b9d58f5523cc9ef75bb94e1bd9093d79b1
wookyoungK <pd1125@naver.com>
2023-02-01 01:07:35 +0900

    연관관계 매핑 - 일대일[1:1]

--- ex1-hello-jpa/src/main/java/hellojpa/Locker.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Locker.java
@@ -3,6 +3,7 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Locker {
@@ -10,4 +11,7 @@ public class Locker {
     @Id @GeneratedValue
     private Long id;
     private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
 }
